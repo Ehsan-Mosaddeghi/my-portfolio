8739cfec128b98023b9aad5394b8b17a76971b84
Ehsan <xdehsanxd@gmail.com>
2025-02-20 13:37:06 +0330

    Update DownloadResumeBtn component for improved download functionality and adjust PDF headers for correct content type

--- app/_components/DownloadResumeBtn.tsx
+++ app/_components/DownloadResumeBtn.tsx
@@ -4,7 +4,7 @@ const DownloadResumeBtn = () => {
   return (
     <a
       href="/Ehsan-Mosaddeghi-Resume.pdf"
-      download="/Ehsan-Mosaddeghi-Resume.pdf"
+      download
       className="bg-blue-400 flex gap-2 items-center overflow-hidden  px-4 py-2 rounded-xl z-10 border-2 border-blue-700 relative before:block before:absolute before:w-full before:h-full before:top-0 duration-200  before:-left-full before:bg-gradient-to-r before:from-transparent hover:shadow-xl before:via-slate-300 before:to-transparent before:transition-all before:duration-700 hover:before:left-full hover:text-gray-200 hover:bg-blue-500"
     >
       <Download /> Download Resume
