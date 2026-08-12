90a32270879b66aabc8fc9bbbf8152e1ce7227a0
Ehsan <xdehsanxd@gmail.com>
2026-08-12 18:50:08 +0330

    Update Footer component to dynamically display the current year; clean up Hero component formatting and remove console log

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -1,11 +1,13 @@
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
     <>
       <footer className="w-full py-8 border-t border-border text-center space-y-2 bg-secondary">
         <h2 className="text-xl">
           <strong className="text-primary">Ehsan</strong> Mosaddeghi
         </h2>
-        <p>© 2025 , All rights reserved.</p>
+        <p>© {currentYear} , All rights reserved.</p>
       </footer>
       <div className="w-full h-2 bg-gradient-to-r from-blue-300 via-blue-600 to-blue-300"></div>
     </>
--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -1,17 +1,14 @@
-'use client'
+"use client";
 
 import DownloadResumeBtn from "@/app/_components/DownloadResumeBtn";
 import { BlurFade } from "@/components/ui/blur-fade";
 import { useEffect, useState } from "react";
-
-
 
 const Hero = () => {
   const [age, setAge] = useState<number>(20);
 
   useEffect(() => {
     const currentYear = new Date().getFullYear();
-    console.log(currentYear);
 
     setAge(currentYear - 2005);
   }, [setAge]);
@@ -22,11 +19,16 @@ const Hero = () => {
       duration={0.6}
       direction="up"
     >
-      <h1 className="text-4xl md:text-6xl font-semibold text-shadow ">Ehsan Mosaddeghi</h1>
-      <h1 className="text-4xl md:text-6xl font-semibold text-shadow">Front-End Developer</h1>
+      <h1 className="text-4xl md:text-6xl font-semibold text-shadow ">
+        Ehsan Mosaddeghi
+      </h1>
+      <h1 className="text-4xl md:text-6xl font-semibold text-shadow">
+        Front-End Developer
+      </h1>
       <p className="opacity-60 text-lg max-w-xl text-center">
-        {age}-year-old self-taught frontend developer with a passion for learning and crafting innovative user experiences.
-        Enthusiastic about growth and eager to explore new technologies.
+        {age}-year-old self-taught frontend developer with a passion for
+        learning and crafting innovative user experiences. Enthusiastic about
+        growth and eager to explore new technologies.
       </p>
       <DownloadResumeBtn />
     </BlurFade>
